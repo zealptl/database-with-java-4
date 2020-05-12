ca37d3be422507d72dfb79ef8b26facb3743a116
zealptl <zealpatel06@gmail.com>
2020-05-11 21:44:08 -0400

    added more entries to table

---
+++ src/sample/RetriveData.java
@@ -0,0 +1,13 @@
+package sample;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class RetriveData {
+//    public static ResultSet getData(String course) throws Exception {
+//        Connection conn = ConnectDB.getConnection();
+//        String query = "select courseID";
+//        PreparedStatement stmt = conn.prepareStatement();
+//    }
+}
